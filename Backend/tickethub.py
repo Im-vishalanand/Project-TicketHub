bb7db71afa29c301e40380944c9ab662608f189f
Kumar Vishal Anand <vishalanand270@gmail.com>
2023-07-21 11:38:58 +0530

    new routes added

--- Backend/tickethub.py
+++ Backend/tickethub.py
@@ -180,6 +180,342 @@ def user_login() :
 
 
 
+# get movie
+# http://127.0.0.1:5000/movie/get
+@app.route("/movies/get", methods=["GET"])
+def get_all_movies():
+    all_movies = Movie.objects()
+    return all_movies.to_json(), 200
+
+# add movie
+# http://127.0.0.1:5000/movie/add
+@app.route("/movie/add", methods=["POST"])
+def add_movie():
+    new_movie = request.get_json()
+    movie = Movie(**new_movie)
+    movie.save()
+    return movie.to_json(), 201
+
+
+
+
+# add event
+# http://127.0.0.1:5000/event/add
+@event_routes.route("/event/add", methods=["POST"])
+def add_event () :
+    new_event = request.get_json()
+    movie = Event(**new_event)
+    movie.save()
+    return movie.to_json(), 201
+
+
+
+
+# get all the shows
+# http://127.0.0.1:5000/event_shows
+@app.route("/event_shows", methods=["GET"])
+def get_all_shows():
+    event_shows = Event_Show.objects()
+    updated_event_shows = []
+
+    for event_show in event_shows:
+        # Fetch the corresponding Event object using the event_id attribute of event_show
+        event = Event.objects.with_id(event_show.event_id.id)
+
+        if event:
+            # Calculate the end time using the helper function
+            end_time = calculate_end_time(event_show.start_time, event.duration)
+
+            # Create a new dictionary and copy the fields from event_show and event
+            updated_event_show = {
+                **event_show.to_mongo(),
+                "image_url": event.image_url,
+                "duration": event.duration,
+                "event_name": event.event_name,
+                "end_time": end_time,
+                "event_id": str(event.id),  # Convert the ObjectId to a string
+            }
+            updated_event_shows.append(updated_event_show)
+
+    # Convert the ObjectId to a string for each item in the list
+    for show in updated_event_shows:
+        show["_id"] = str(show["_id"])
+
+    # Return the Response object with a valid JSON response
+    return Response(response=json.dumps(updated_event_shows), status=200, mimetype="application/json")
+
+
+
+# get all the shows related to a particular event
+# http://127.0.0.1:5000/event_shows/id
+@app.route("/event_shows/<ObjectId:_id>", methods=["GET"])
+def get_related_shows(_id):
+    # Fetch the Event_Show objects related to the specified Event _id
+    event_shows = Event_Show.objects(event_id=_id)
+    print(event_shows)
+    # Fetch the corresponding Event object
+    event = Event.objects.with_id(_id)  # Use with_id() to fetch the event with the given ObjectId
+
+    if not event:
+        return jsonify({"message": "Event not found"}), 404
+
+    # Create a list to store updated Event_Show objects with additional fields
+    updated_event_shows = []
+
+    # Update each Event_Show object with image_url and duration from the Event object
+    for event_show in event_shows:
+        # Calculate the end time using the helper function
+        end_time = calculate_end_time(event_show.start_time, event.duration)
+
+        # Create a new dictionary and copy the fields from event_show and event
+        updated_event_show = {
+            **event_show.to_mongo(),
+            "image_url": event.image_url,
+            "duration": event.duration,
+            "event_name": event.event_name,
+            "end_time": end_time,
+            "event_id": str(event.id),  # Convert the ObjectId to a string
+        }
+        updated_event_shows.append(updated_event_show)
+
+    # Convert the ObjectId to a string for each item in the list
+    for show in updated_event_shows:
+        show["_id"] = str(show["_id"])
+
+    return jsonify(updated_event_shows)
+
+
+
+# create a new show
+# http://127.0.0.1:5000/event_show/create
+@app.route("/event_show/create", methods=["POST"])
+def create_event_show():
+    new_event_show = request.get_json()
+    event = Event.objects(id=new_event_show["event_id"]).first()
+    
+    # Calculate the end time using the helper function
+    end_time = calculate_end_time(new_event_show["start_time"], event.duration)
+    
+    # Create the Event_Show object and set the end_time field
+    event_show = Event_Show(
+        event_id=new_event_show["event_id"],
+        date=new_event_show["date"],
+        language=new_event_show["language"],
+        price=new_event_show["price"],
+        start_time=new_event_show["start_time"],
+        end_time=end_time,  # Set the calculated end time
+        total_seats=new_event_show.get("total_seats", 100),
+        booked_seats=new_event_show.get("booked_seats", 0),
+        seat_map=new_event_show.get("seat_map", [[0] * 10 for _ in range(10)])  # Default 10x10 seat_map
+    )
+    
+    event_show.save()
+
+    event.shows.append(event_show.id)
+    event.save()
+    return event_show.to_json(), 201
+
+
+# Book a show
+# http://127.0.0.1:5000/event_show/id
+@app.route("/book_event_show/<ObjectId:_id>", methods=["PUT"])
+def book_event_show(_id):
+    user_id = g.user_id
+    booked_show_data = request.get_json()
+    
+    # Find the existing Event_Show object by _id
+    event_show = Event_Show.objects.with_id(_id)
+    # return event_show.to_json()
+
+    if not event_show:
+        return jsonify({"message": "Event_Show not found"}), 404
+    
+    def count_non_zero_values(arr):
+        count = 0
+        for row in arr:
+            for value in row:
+                if value != 0:
+                    count += 1
+        return count
+
+    # Update the fields of the existing Event_Show object with the new data
+    for key, value in booked_show_data.items():
+        if key == "event_id" or key == "_id" :
+            continue
+        elif key == "seat_map" :
+            setattr(event_show, key, value)
+            count = count_non_zero_values(value)
+            setattr(event_show, "booked_seats", count)
+        else :
+            setattr(event_show, key, value)
+
+    event_show.save()
+
+    user = User.objects.with_id(user_id)
+    user.event_show_bookings.append(_id)
+     # Convert the event_show price to Decimal
+    event_show_price = Decimal(str(event_show.price))
+
+    user.wallet_balance = user.wallet_balance - event_show_price
+    user.save()
+
+    return event_show.to_json()
+
+
+
+
+# get all the shows
+# http://127.0.0.1:5000/movie_shows
+@app.route("/movie_shows", methods=["GET"])
+def get_all_shows():
+    movie_shows = Movie_Show.objects()
+    updated_movie_shows = []
+
+    for movie_show in movie_shows:
+        # Fetch the corresponding Movie object using the movie_id attribute of movie_show
+        movie = Movie.objects.with_id(movie_show.movie_id.id)
+
+        if movie:
+            # Calculate the end time using the helper function
+            end_time = calculate_end_time(movie_show.start_time, movie.duration)
+
+            # Create a new dictionary and copy the fields from movie_show and movie
+            updated_movie_show = {
+                **movie_show.to_mongo(),
+                "image_url": movie.image_url,
+                "duration": movie.duration,
+                "movie_name": movie.movie_name,
+                "end_time": end_time,
+                "movie_id": str(movie.id),  # Convert the ObjectId to a string
+            }
+            updated_movie_shows.append(updated_movie_show)
+
+    # Convert the ObjectId to a string for each item in the list
+    for show in updated_movie_shows:
+        show["_id"] = str(show["_id"])
+
+    # Return the Response object with a valid JSON response
+    return Response(response=json.dumps(updated_movie_shows), status=200, mimetype="application/json")
+
+
+# get all the shows related to a particular movie
+# http://127.0.0.1:5000/movie_shows/<ObjectId:_id>
+@app.route("/movie_shows/<ObjectId:_id>", methods=["GET"])
+def get_related_shows(_id):
+    # Fetch the Movie_Show objects related to the specified Movie _id
+    movie_shows = Movie_Show.objects(movie_id=_id)
+    print(movie_shows)
+    # Fetch the corresponding Movie object
+    movie = Movie.objects.with_id(_id)  # Use with_id() to fetch the movie with the given ObjectId
+
+    if not movie:
+        return jsonify({"message": "Movie not found"}), 404
+
+    # Create a list to store updated Movie_Show objects with additional fields
+    updated_movie_shows = []
+
+    # Update each Movie_Show object with image_url and duration from the Movie object
+    for movie_show in movie_shows:
+        # Calculate the end time using the helper function
+        end_time = calculate_end_time(movie_show.start_time, movie.duration)
+
+        # Create a new dictionary and copy the fields from movie_show and movie
+        updated_movie_show = {
+            **movie_show.to_mongo(),
+            "image_url": movie.image_url,
+            "duration": movie.duration,
+            "movie_name": movie.movie_name,
+            "end_time": end_time,
+            "movie_id": str(movie.id),  # Convert the ObjectId to a string
+        }
+        updated_movie_shows.append(updated_movie_show)
+
+    # Convert the ObjectId to a string for each item in the list
+    for show in updated_movie_shows:
+        show["_id"] = str(show["_id"])
+
+    return jsonify(updated_movie_shows)
+
+
+# create a new show
+# http://127.0.0.1:5000/movie_show/create
+@app.route("/movie_show/create", methods=["POST"])
+def create_movie_show():
+    new_movie_show = request.get_json()
+    movie = Movie.objects(id=new_movie_show["movie_id"]).first()
+
+    # Calculate the end time using the helper function
+    end_time = calculate_end_time(new_movie_show["start_time"], movie.duration)
+
+    # Create the Movie_Show object and set the end_time field
+    movie_show = Movie_Show(
+        movie_id=new_movie_show["movie_id"],
+        date=new_movie_show["date"],
+        language=new_movie_show["language"],
+        price=new_movie_show["price"],
+        start_time=new_movie_show["start_time"],
+        end_time=end_time,  # Set the calculated end time
+        total_seats=new_movie_show.get("total_seats", 100),
+        booked_seats=new_movie_show.get("booked_seats", 0),
+        seat_map=new_movie_show.get(
+            "seat_map", [[0] * 10 for _ in range(10)]
+        ),  # Default 10x10 seat_map
+    )
+
+    movie_show.save()
+
+    movie.shows.append(movie_show.id)
+    movie.save()
+    return movie_show.to_json(), 201
+
+
+# Book a show
+# http://127.0.0.1:5000/book_movie_show/<ObjectId:_id>
+@app.route("/book_movie_show/<ObjectId:_id>", methods=["PUT"])
+def book_movie_show(_id):
+    user_id = g.user_id
+    booked_show_data = request.get_json()
+    
+    # Find the existing Movie_Show object by _id
+    movie_show = Movie_Show.objects.with_id(_id)
+    # return movie_show.to_json()
+
+    if not movie_show:
+        return jsonify({"message": "Movie_Show not found"}), 404
+
+    def count_non_zero_values(arr):
+        count = 0
+        for row in arr:
+            for value in row:
+                if value != 0:
+                    count += 1
+        return count
+    # Update the fields of the existing Movie_Show object with the new data
+    for key, value in booked_show_data.items():
+        if key == "movie_id" or key == "_id" :
+            continue
+        elif key == "seat_map" :
+            setattr(movie_show, key, value)
+            count = count_non_zero_values(value)
+            setattr(movie_show, "booked_seats", count)
+        else :
+            setattr(movie_show, key, value)
+
+    movie_show.save()
+
+    user = User.objects.with_id(user_id)
+    user.movie_show_bookings.append(_id)
+     # Convert the movie_show price to Decimal
+    movie_show_price = Decimal(str(movie_show.price))
+
+    user.wallet_balance = user.wallet_balance - movie_show_price
+    user.save()
+
+    return movie_show.to_json()
+
+
+
+
 # Start the Flask application
 if __name__ == '__main__':
     app.run(debug=True)
