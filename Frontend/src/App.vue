c4e163c8a196156f3bbb9125929622fd2eb82fa0
Kumar Vishal Anand <vishalanand270@gmail.com>
2023-07-21 20:05:46 +0530

    frontend updated

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,30 +1,44 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="vue-tempalte">
+    <!-- Navigation -->
+    <nav
+      class="navbar shadow bg-white rounded justify-content-between flex-nowrap flex-row fixed-top"
+    >
+      <div class="container">
+        <a
+          class="navbar-brand float-left"
+          href="./assets/project-logo.png"
+          target="_blank"
+        >
+          TicketHub
+        </a>
+        <ul class="nav navbar-nav flex-row float-right">
+          <li class="nav-item">
+            <router-link class="nav-link pr-3" to="/login">Sign in</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="btn btn-outline-primary" to="/"
+              >Sign up</router-link
+            >
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- Main -->
+    <div class="App">
+      <div class="vertical-center">
+        <div class="inner-block">
+          <router-view />
+        </div>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
+<style>
+  .nav-link{
+    margin-right: 10px;
+  }
 </style>
